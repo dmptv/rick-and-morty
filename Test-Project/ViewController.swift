af50cba43508fcdb8b83fb7acdbf497d4d96eb21
Kanat.Absadykov <kanat.absadykov@kcell.kz>
2021-01-05 17:32:57 +0600

    Initial Commit

---
+++ Test-Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test-Project
+//
+//  Created by Kanat on 05.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
